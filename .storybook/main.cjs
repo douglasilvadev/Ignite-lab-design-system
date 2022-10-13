476d91ee2152dd5cb539cf06fa6194cb10b1a98d
Douglas Silva <douglasilvadev@gmail.com>
2022-10-13 11:51:33 -0300

    Fix infos for deploying

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react",
   "core": {
@@ -19,6 +20,7 @@ module.exports = {
     if (configType === 'PRODUCTION') {
       config.base = '/Ignite-lab-design-system/'
     }
+
     return config
   }
 }
